8f4a9ca37f51962fc40dff7dbd2e4ab7a66fcdd1
inna <inna.sokolova@intermobility.ru>
2025-02-04 19:39:12 +0500

    test

---
+++ src/main/groovy/org/example/CreateMap.groovy
@@ -0,0 +1,22 @@
+package org
+
+class CreateMap {
+  static Map<Integer, Long> countElements(List<Integer> list) {
+    list.groupBy { it }.collectEntries { k, v -> [k, v.size() as Long] }
+  }
+
+  static Map<Integer, Long> countElements(Integer[] array) {
+    Arrays.asList(array).groupBy { it }.collectEntries { k, v -> [k, v.size() as Long] }
+  }
+
+
+  static void main(String[] args) {
+    def numbersList = [1, 3, 4, 5, 1, 5, 4]
+    def countsList = countElements(numbersList)
+    println countsList
+
+    Integer[] numbersArr = [1, 3, 4, 5, 1, 5, 4] as Integer[]
+    def countsArr = countElements(numbersArr)
+    println countsArr
+  }
+}
